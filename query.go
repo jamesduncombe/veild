9d62dda0dd6a7bdc73a69a1b8a8bad5c37070210
James Duncombe <james@jamesduncombe.com>
2025-04-23 10:45:22 +0100

    Moves decTTL and getTTLs to Query methods.

---
+++ query.go
@@ -0,0 +1,44 @@
+package veild
+
+import (
+	"encoding/binary"
+	"time"
+)
+
+// Query holds the structure for the raw response data and offsets of TTLs.
+type Query struct {
+	data     []byte
+	offsets  []int
+	creation time.Time
+}
+
+func (q Query) cacheKey() uint64 {
+	nameType, _ := sliceNameType(q.data[12:])
+	return createCacheKey(nameType)
+}
+
+// decTTL decrements a responses TTL by n seconds.
+func (q Query) decTTL(decrementBy uint32) (Query, bool) {
+	for _, offset := range q.offsets {
+		currentTTL := binary.BigEndian.Uint32(q.data[offset : offset+4])
+
+		// If we're decrementing to 0 or past 0 then the record should expire.
+		if decrementBy >= currentTTL {
+			return q, false
+		}
+
+		// Update TTL.
+		binary.BigEndian.PutUint32(q.data[offset:offset+4], currentTTL-decrementBy)
+	}
+	return q, true
+}
+
+// getTTLs gets the TTLs from the offsets using the data.
+func (q Query) getTTLs() []uint32 {
+	ttls := []uint32{}
+	for _, offset := range q.offsets {
+		currentTTL := binary.BigEndian.Uint32(q.data[offset : offset+4])
+		ttls = append(ttls, currentTTL)
+	}
+	return ttls
+}
---
+++ query_test.go
@@ -0,0 +1,50 @@
+package veild
+
+import (
+	"encoding/binary"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestQuery_cacheKey(t *testing.T) {
+	t.Skip()
+}
+
+func TestQuery_decTTL(t *testing.T) {
+	file, _ := os.ReadFile("fixtures/client.dropbox.com_aaaa.pkt")
+	offsets, _ := ttlOffsets(file)
+
+	originalTtls := []uint32{}
+
+	for _, offset := range offsets {
+		originalTtls = append(originalTtls, binary.BigEndian.Uint32(file[offset:offset+4]))
+	}
+
+	query := Query{offsets: offsets, data: file}
+
+	newQuery, _ := query.decTTL(1)
+
+	for i, offset := range offsets {
+		got := binary.BigEndian.Uint32(newQuery.data[offset : offset+4])
+		want := originalTtls[i] - 1
+
+		// Check TTL is decremented by 1.
+		if want != got {
+			t.Errorf("wanted %v got %v", want, got)
+		}
+	}
+}
+
+func TestQuery_getTTLs(t *testing.T) {
+	file, _ := os.ReadFile("fixtures/client.dropbox.com_aaaa.pkt")
+	offsets, _ := ttlOffsets(file)
+
+	query := Query{data: file, offsets: offsets}
+	got := query.getTTLs()
+	want := []uint32{31, 60}
+
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("wanted %v got %v", want, got)
+	}
+}
